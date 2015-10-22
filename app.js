be4c05d2f74b5fddf68997271cbd9fad36629be5
Mike Boeser <mboeser@yahoo.com>
2015-10-21 19:41:53 -0500

    completed version

--- app.js
+++ app.js
@@ -2,31 +2,44 @@ var total = 0;
 
 $(document).ready(function(){
 
-	
+	$('#genTotal').text('Total Clicks: ' +total);	
 
-$('#container').on('click', '#genButton', function(){
- addButtons();
-});
+	$('#container').on('click', '#genButton', function(){
+ 		addButtons();
+
+	});
 	
 deleteButton();
+
+changeButton();
 
 });
 
 function addButtons (){
 	
 	total++
-	$('#genTotal').text(total);
+	$('#genTotal').text('Total Clicks: ' +total);
 
 
 	$("#container").append("<div class='btnGroup'></div>");
 		var $el = $("#container").children().last();
-		$el.append('<button id="changeButton">CHANGE</button>');
-		$el.append('<button id="deleteButton">DELETE</button>');
+		$el.append('<button class="changeButton">CHANGE</button>');
+		$el.append('<button class="deleteButton">DELETE '+ total+'</button>');
 		
 }
 
 function deleteButton () {
-	$('#container').on('click', '#deleteButton', function(){
+
+	$('#container').on('click', '.deleteButton', function(){
 		$(this).closest('.btnGroup').remove();
+
+		total--
+	$('#genTotal').text('Total Clicks: ' +total);
 });
+}
+
+function changeButton () {
+	$('#container').on('click', '.changeButton', function(){
+	$('body').toggleClass('bkgdColor2');
+	});
 }
